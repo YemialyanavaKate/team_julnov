eb4a6bf6211346ee7fddb39e287a981bd7547ea1
YemialyanavaKate <yemialyanavakate@gmail.com>
2024-11-12 18:02:41 +0000

    ITA: yemialyanava 3-2

--- web-app/src/main/java/by/ita/je/service/FridgeWebService.java
+++ web-app/src/main/java/by/ita/je/service/FridgeWebService.java
@@ -5,6 +5,8 @@ import by.ita.je.mappers.FridgeMapper;
 import by.ita.je.models.Fridge;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -13,7 +15,7 @@ public class FridgeWebService {
     private final RestTemplate serviceAppRestClient;
     private final FridgeMapper fridgeMapper;
 
-    public Fridge createFridgeDto(FridgeWebDto fridgeWebDto){
+    public Fridge createFridgeDto(FridgeWebDto fridgeWebDto) throws HttpClientErrorException, HttpServerErrorException {
         String urlFridgeCreate = "http://127.0.0.1:8111/business/fridge/create";
         return fridgeMapper.toEntity(serviceAppRestClient.postForObject(urlFridgeCreate, fridgeWebDto, FridgeWebDto.class));
     }
