c9fc296d79f5dc33bad2447f2100404f271be14b
Kate Yemialyanava <yemialyanavakate@gmail.com>
2024-11-25 22:28:41 +0300

    HW 5-1

---
+++ web-app/src/main/java/by/ita/je/controller/AdminController.java
@@ -0,0 +1,16 @@
+package by.ita.je.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("/dashboard")
+    public String adminDashboard(){
+        return "admin-dashboard.html";
+    }
+}
